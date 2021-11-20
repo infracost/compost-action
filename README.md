644b3063ea010973eedb3bd885d55d7c53de8e76
tim775 <52185+tim775@users.noreply.github.com>
2021-11-19 22:34:12 -0500

    feat: Add action

---
+++ README.md
@@ -0,0 +1,42 @@
+# Infracost Compost Action
+
+Post pull request comments from multiple CI platforms.
+
+This GitHub Action posts a GitHub comment to a commit or pull request.
+
+## Usage
+
+```yaml
+on: [pull_request]
+jobs:
+  compost:
+    runs-on: ubuntu-latest
+    name: Post comment
+    steps:
+      - name: Comment
+        uses: infracost/compost@master
+        with: 
+          body: "This is a compost comment!"
+```
+
+## Inputs
+
+The action supports the following inputs:
+
+- `behavior`: Optional, defaults to `update`.  The behavior to use when posting comments.  Must be one of `update` | `delete_and_new` | `hide_and_new` | `new` | `latest`.   
+
+- `body`: Specify the comment body content.
+
+- `bodyFile`: Optional.  Specify a path to a file containing the comment body.  Mutually exclusive with body.
+
+- `tag`: Optional.  Customize the comment tag. This is added to the comment as a markdown comment to detect the previously posted comments.
+
+- `targetType`: Optional, defaults to `pr`.  What object should be commented on.  Must be `pr` or `commit`.  When `commit`, the `behavior` option will be ignored.
+
+- `repository`: Optional, defaults to `${{ github.repository }}`.  The GitHub owner and repository name. For example, 'infracost/compost-action'.
+
+- `pullRequestNumber:`: Optional, defaults to the pull request number of the current event..
+
+## Outputs
+
+- `latest`: The body of the latest existing comment when using behavior 'latest'.
---
+++ src/index.ts
@@ -0,0 +1,116 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import Compost from '@infracost/compost';
+import fs from 'fs';
+import { GetBehavior, PostBehavior } from '@infracost/compost/dist/types';
+
+const validBehavior = [
+  'update',
+  'delete_and_new',
+  'hide_and_new',
+  'new',
+  'latest',
+];
+const validTargetType = ['pr', 'comment'];
+
+function loadBody(): string {
+  const body = core.getInput('body');
+  if (body) {
+    return body;
+  }
+
+  const bodyFile = core.getInput('bodyFile');
+  if (!bodyFile) {
+    throw new Error('body or bodyFile is required');
+  }
+
+  if (!fs.existsSync(bodyFile)) {
+    throw new Error(`body-file ${bodyFile} does not exist`);
+  }
+
+  try {
+    return fs.readFileSync(bodyFile, 'utf8');
+  } catch (err) {
+    throw new Error(`Error reading body-file: ${err as string}`);
+  }
+}
+
+async function comment(): Promise<void> {
+  try {
+    const behavior = core.getInput('behavior', { required: true });
+    if (!validBehavior.includes(behavior)) {
+      throw new Error(
+        `Invalid behavior '${behavior}' must be one of: ${validBehavior.join(
+          ', '
+        )}`
+      );
+    }
+
+    const targetType = core.getInput('targetType', { required: true });
+    if (!validTargetType.includes(targetType)) {
+      throw new Error(
+        `Invalid targetType '${targetType}' must be one of: ${validTargetType.join(
+          ', '
+        )}`
+      );
+    }
+
+    const tag = core.getInput('tag');
+
+    const repository =
+      core.getInput('repository') || process.env.GITHUB_REPOSITORY;
+    if (!repository) {
+      throw new Error('repository is required');
+    }
+
+    const compost = new Compost({
+      tag,
+      logger: { debug: core.debug, warn: core.warning, info: core.info },
+    });
+
+    let targetRef: string | number | undefined;
+    if (targetType === 'pr') {
+      targetRef =
+        core.getInput('pullRequestNumber') ||
+        github.context?.payload?.pull_request?.number;
+    } else if (targetType === 'commit') {
+      targetRef = process.env.GITHUB_SHA;
+    } else {
+      throw new Error('targetType is required');
+    }
+
+    if (!targetRef) {
+      throw new Error(`No target found for targetType '${targetType}'`);
+    }
+
+    if (behavior === 'latest') {
+      const latestComment = await compost.getComment(
+        'github',
+        repository,
+        targetType,
+        targetRef,
+        behavior as GetBehavior
+      );
+      if (latestComment) {
+        core.setOutput('latest', latestComment.body);
+      }
+    } else {
+      const body = loadBody();
+      await compost.postComment(
+        'github',
+        repository,
+        targetType,
+        targetRef,
+        behavior as PostBehavior,
+        body
+      );
+    }
+  } catch (e) {
+    core.setFailed(e as string | Error);
+  }
+}
+
+if (require.main === module) {
+  // eslint-disable-next-line no-void
+  void comment();
+}
