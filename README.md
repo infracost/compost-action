890869e23cf8a4493f3836a1719a386c88441700
Tim McFadden <52185+tim775@users.noreply.github.com>
2021-11-22 13:22:05 -0500

    feat: Add action (#1)
    * feat: Add action
    
    * fix: fix path
    
    * fix: Add GITHUB_TOKEN
    
    * fix: typo
    
    * fix: stripMarkdownTag
    
    * fix: npm run prepare
    
    * test: Add body-file test
    
    * fix: move testdata
    
    * fix: Add newline in test expectation
    
    * fix: newline expectation
    
    * fix: add shell
    
    * test: fix expected
    
    * test: add newline
    
    * test: printf
    
    * fix: try to strip newlines for comparison
    
    * fix: Update example to point at correct repo
    
    * docs: Update interface docs
    
    * docs: repository -> repo
    
    * feat: Update code to match new interface
    
    * refactor: add debug log
    
    * fix: Set GITHUB_TOKEN env before detecting github action

---
+++ README.md
@@ -0,0 +1,44 @@
+# Infracost Compost Action
+
+Post pull request comments from multiple CI platforms.
+
+This GitHub Action posts a GitHub comment to a commit or pull request.
+
+## Usage
+
+```yaml
+on: [pull_request]
+jobs:
+  compost:
+    runs-on: ubuntu-latest
+    name: Post comment
+    steps:
+      - name: Comment
+        uses: infracost/compost-action@master
+        with: 
+          body: "This is a compost comment!"
+```
+
+## Inputs
+
+The action supports the following inputs:
+
+- `behavior`: Optional, defaults to `update`. The behavior to use when posting comments. Must be one of `update` | `delete_and_new` | `hide_and_new` | `new` | `latest`.  
+
+- `body`: Specify the comment body content.
+
+- `bodyFile`: Optional. Specify a path to a file containing the comment body. Mutually exclusive with body.
+
+- `tag`: Optional. Customize the comment tag. This is added to the comment as a markdown comment to detect the previously posted comments.
+
+- `repo`: Optional, defaults to the `GITHUB_REPOSITORY` env. The GitHub owner and repository name. For example, 'infracost/compost-action'.
+
+- `targetType`: Optional. Which objects should be commented on. May be `pr` or `commit`.
+
+- `prNumber`: Optional, defaults to the pull request of the current event. The number of the pull request to be commented on.
+
+- `GITHUB_TOKEN`: Optional, default to `${{ github.token }}`.
+
+## Outputs
+
+- `latest`: The body of the latest existing comment when using behavior 'latest'.
---
+++ src/index.ts
@@ -0,0 +1,145 @@
+import * as core from '@actions/core';
+import Compost from '@infracost/compost';
+import fs from 'fs';
+import {
+  DetectResult,
+  GetBehavior,
+  PostBehavior,
+  TargetType,
+} from '@infracost/compost/dist/types';
+import { GitHubOptions } from '@infracost/compost/dist/platforms/github';
+import { stripMarkdownTag } from '@infracost/compost/dist/util';
+import { GitHubActionsDetector } from '@infracost/compost/dist/detect/githubActions';
+import { DetectError } from '@infracost/compost/dist/detect';
+
+const validBehavior = [
+  'update',
+  'delete_and_new',
+  'hide_and_new',
+  'new',
+  'latest',
+];
+const validTargetType = ['pr', 'comment'];
+
+function loadBody(): string {
+  const body = core.getInput('body');
+  if (body) {
+    return body;
+  }
+
+  const bodyFile = core.getInput('bodyFile');
+  if (!bodyFile) {
+    throw new Error('body or bodyFile is required');
+  }
+
+  if (!fs.existsSync(bodyFile)) {
+    throw new Error(`body-file ${bodyFile} does not exist`);
+  }
+
+  try {
+    return fs.readFileSync(bodyFile, 'utf8');
+  } catch (err) {
+    throw new Error(`Error reading body-file: ${err as string}`);
+  }
+}
+
+async function comment(): Promise<void> {
+  try {
+    const logger = { debug: core.debug, warn: core.warning, info: core.info };
+
+    const behavior = core.getInput('behavior', { required: true });
+    if (!validBehavior.includes(behavior)) {
+      throw new Error(
+        `Invalid behavior '${behavior}' must be one of: ${validBehavior.join(
+          ', '
+        )}`
+      );
+    }
+
+    const tag = core.getInput('tag');
+
+    const token = core.getInput('GITHUB_TOKEN', { required: true });
+
+    const compost = new Compost({
+      tag,
+      token,
+      logger,
+    } as GitHubOptions);
+
+    const inputTargetType = core.getInput('targetType') as TargetType;
+    if (inputTargetType && !validTargetType.includes(inputTargetType)) {
+      throw new Error(
+        `Invalid targetType '${inputTargetType}' must be one of: ${validTargetType.join(
+          ', '
+        )}`
+      );
+    }
+
+    process.env.GITHUB_TOKEN = token; // Need to set the environment variable because the detector requires it to detect github.
+    const detector = new GitHubActionsDetector({
+      targetTypes: inputTargetType ? [inputTargetType] : undefined,
+      logger,
+    });
+
+    let result: DetectResult = null;
+    try {
+      result = detector.detect();
+      logger.debug(
+        `Detected {platform:'${result.platform}' project:'${result.project}' targetType:'${result.targetType}' targetRef:'${result.targetRef}'}`
+      );
+    } catch (err) {
+      if (err instanceof Error && err.name === DetectError.name) {
+        logger.debug(err.message);
+      } else {
+        throw err;
+      }
+    }
+
+    const repo = core.getInput('repo') || result?.project;
+    if (!repo) {
+      throw new Error('repo could not be detected');
+    }
+
+    const targetType = inputTargetType || result?.targetType;
+    if (!targetType) {
+      throw new Error('targetType could not be detected');
+    }
+
+    const prNumber = core.getInput('prNumber');
+    const commitSha = core.getInput('commitSha');
+    const targetRef = prNumber || commitSha || result?.targetRef;
+    if (!targetRef) {
+      throw new Error('targetRef could not be detected');
+    }
+
+    if (behavior === 'latest') {
+      const latestComment = await compost.getComment(
+        'github',
+        repo,
+        targetType,
+        targetRef,
+        behavior as GetBehavior
+      );
+      if (latestComment) {
+        core.setOutput('latest', stripMarkdownTag(latestComment.body));
+      }
+    } else {
+      const body = loadBody();
+      await compost.postComment(
+        'github',
+        repo,
+        targetType,
+        targetRef,
+        behavior as PostBehavior,
+        body
+      );
+    }
+  } catch (e) {
+    core.setFailed(e as string | Error);
+  }
+}
+
+if (require.main === module) {
+  // eslint-disable-next-line no-void
+  void comment();
+}
